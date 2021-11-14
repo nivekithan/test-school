47121795560c7c93db3fd18593afd785ee1419f7
Nivekithan S <nivekithan363@gmail.com>
2021-11-14 14:16:37 +0530

    Add questions (Drilling Enginneering)

---
+++ src/components/for_exam/drillingEnginneering.tsx
@@ -0,0 +1,267 @@
+import React from "react";
+import { Essay } from "../question/essay";
+import { FillInTheBlanks } from "../question/fillInTheBlanks";
+import { MultipleInputQuestion } from "../question/multipleInputQuestion";
+import { OneLineQuestion } from "../question/oneLineQues";
+import { TrueOrFalse } from "../question/trueOrFalse";
+
+const Lec4 = () => {
+  return (
+    <>
+      <MultipleInputQuestion
+        question="What are the functions of Drilling mud"
+        answer={[
+          "Bottomhole cleaning",
+          "Cuttings transport",
+          "Borehole wall support",
+          "Balancing formation pressure",
+          "Cooling the bit",
+          "Hydraulic power transmission",
+          "Data transmission",
+          "Reducing friction",
+          "Corrosion protection",
+        ]}
+      />
+      <MultipleInputQuestion
+        question="What are different types of drilling mud"
+        answer={["Water based mud", "Oil based mud"]}
+      />
+      <MultipleInputQuestion
+        question="What are criteria for drilling fluid selection"
+        answer={[
+          "Pore pressure to establish the minimum and maximum mud weight",
+          "Offset well data from adjacent wells",
+          "Geological plot of the prognosed lithology",
+          "Casing design programme and casing seat depths",
+          "Basic mud properties for each open hole section before it is cased off",
+          "Restriction that might be enforced in the are",
+        ]}
+      />{" "}
+      <MultipleInputQuestion
+        question="What will happen if the drilled cuttings are not removed from borehole"
+        answer={[
+          "Reduction in Drilling Efficiency",
+          "Changes in Rheological properties of fluids",
+        ]}
+      />
+      <MultipleInputQuestion
+        question="What are the functions of Drilling fluids regarding removing cuttings"
+        answer={[
+          "Carry the cuttings to surface while circulating",
+          "Suspend the cuttings while not circulating",
+          "Drop the cuttings out of suspension at surface",
+        ]}
+      />
+      <MultipleInputQuestion
+        question={[
+          "The carrying capacity of the mud depends on the",
+          ",",
+          "and",
+          "of the mud",
+        ]}
+        answer={["Annular velocity", "Density", "Viscosity"]}
+      />
+      <FillInTheBlanks
+        question={[
+          "Ability to suspend the cuttings depends on",
+          "properties of the mud",
+        ]}
+        answer={["Thixotropic"]}
+      />
+      <MultipleInputQuestion
+        question={[
+          "The drilled solids are removed at the surface by devices such as",
+          ",",
+          "and",
+          "",
+        ]}
+        answer={["Shale Shakers", "Desanders", "Desilters"]}
+      />
+      <TrueOrFalse
+        question="It is ecnomically feasible to remove all the drilled cuttings before recirculating the mud"
+        answer={false}
+      />
+      <OneLineQuestion
+        question="Why less hydrostatic pressure exerted by mud is not desirable"
+        answer="Can cause influx of formation fluids into wellbore"
+      />
+      <OneLineQuestion
+        question="Why high hydrostatic pressure exerted by mud is not desirable"
+        answer="Can cause formation to fracture"
+      />
+      <OneLineQuestion
+        question="What is lost circulation"
+        answer="Flow of mud into the formation while drilling"
+      />
+      <FillInTheBlanks
+        question={[
+          "Mud weight has to be selected so that it exceeds the",
+          "pressure but does not exceed",
+          "pressure of the formation being penetrated",
+        ]}
+        answer={["Pore", "Fracture"]}
+      />
+      <MultipleInputQuestion
+        question={[
+          "",
+          "and",
+          "is added to viscosified mud as weighting material",
+        ]}
+        answer={["Barite", "Haemitite"]}
+      />
+      <Essay
+        question="Why high density material are used as weighting material"
+        points={[
+          [
+            "Material such as",
+            "and",
+            "with high density are used because then only",
+            "of these minerals needed to be added to the mud to",
+            "the overall",
+            "of the mud",
+          ],
+          [
+            "As a result the impact of weighting material on the",
+            "properties of the mud will be",
+            "",
+          ],
+        ]}
+        answer={[
+          ["Barite", "Haemitite", "lower volume", "increase", "density"],
+          ["Rheological", "minimised"],
+        ]}
+      />
+      <OneLineQuestion
+        question="What is filter cake"
+        answer="Layer of concentrated solids from the drilling mud formed during natural filtration on the sides of borehole"
+      />
+      <MultipleInputQuestion
+        question="What are different names for filter cake"
+        answer={["Wall cake", "Mud cake"]}
+      />
+      <OneLineQuestion
+        question="What is filtrate"
+        answer="Fluid flowed through filter"
+      />
+      <MultipleInputQuestion
+        question="Loss of mud can result in"
+        answer={[
+          "Deposition of sand particles",
+          "Hydration of clays in pore spaces",
+        ]}
+      />
+      <MultipleInputQuestion
+        question="Lose of filtrate can cause"
+        answer={["Hydration of clays in pore spaces"]}
+      />
+      <OneLineQuestion
+        question="What does hydration of clays leads to"
+        answer="Reduction of permeability"
+      />
+      <FillInTheBlanks
+        question={["The filter cake can become so thick that it may cause", ""]}
+        answer={["Stuck pipe"]}
+      />
+      <MultipleInputQuestion
+        question={["Ideal filter cake is", "and", ""]}
+        answer={["Thin", "Impermeable"]}
+      />
+      <MultipleInputQuestion
+        question={[
+          "Pressure in the wellbore will be equal to P =",
+          "x",
+          "x",
+          "",
+        ]}
+        answer={["0.052", "MW", "TVD"]}
+      />
+      <OneLineQuestion
+        question={[
+          "Data from",
+          "will be useful in predicting borehole stability problems",
+        ]}
+        answer="Adjacent wells"
+      />
+      <OneLineQuestion
+        question={[
+          "",
+          "is one of the most common problems in drilling operations",
+        ]}
+        answer="Shale Instability"
+      />
+      <MultipleInputQuestion
+        question="What are the reasons for shale instability"
+        answer={[
+          "Pressure Differential between bottomhole pressure and the pore pressure",
+          "Hydration of the clays within shale by mud filtrate containing water",
+        ]}
+      />
+      <OneLineQuestion
+        question="How to overcome the pressure differential between bottomhole pressure and the pore pressure"
+        answer="Increasing the mudweight"
+      />
+      <MultipleInputQuestion
+        question="How to overcome the hydration of clay"
+        answer={[
+          "Using non water based muds",
+          "Treating mud with chemicals to reduce the ability of water to hydrate the clays",
+        ]}
+      />
+      <OneLineQuestion
+        question="What is inhibited mud"
+        answer="Mud treated with chemicals to reduce the ability of water to hydrate the clays"
+      />
+      <Essay
+        question="Why it is important to cool the bit"
+        points={[
+          [
+            "The rock cutting process will generate a great deal of",
+            "at the",
+            "",
+          ],
+          ["Unless the", "is", "it will", "and quickly", "out"],
+        ]}
+        answer={[
+          ["heat", "bit"],
+          ["bit", "cooled", "overheat", "wear"],
+        ]}
+      />
+      <FillInTheBlanks
+        question={["The power of mud pumps will be expended in", ""]}
+        answer={["Frictional Pressure"]}
+      />
+      <FillInTheBlanks
+        question={[
+          "The efficiency of drilling process can be significantly improved if approximately",
+          "of power is expended at the",
+          "",
+        ]}
+        answer={["65%", "bit"]}
+      />
+      <MultipleInputQuestion
+        question="What are factors affecting the pressure loss in the system"
+        answer={[
+          "Geometry of system",
+          "viscosity",
+          "mud density",
+          "yield point",
+        ]}
+      />
+      <MultipleInputQuestion
+        question="How can the loss of pressure can be reduced"
+        answer={[
+          "Changing the size of nozzles in the bit",
+          "Changing flowrate through the system",
+        ]}
+      />
+    </>
+  );
+};
+
+export const ForExamDrillingEnginneering = () => {
+  return (
+    <>
+      <Lec4 />
+    </>
+  );
+};

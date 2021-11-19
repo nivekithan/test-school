73b915b2692b77d900ceb39c2cad2a94ae65da55
Nivekithan S <nivekithan363@gmail.com>
2021-11-19 19:46:44 +0530

    Add questions (ITPO)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { ForExamDrillingEnginneering } from "./components/for_exam/drillingEnginneering";
-import { ForExamGeoExpo } from "./components/for_exam/geoExpo";
 import { ForExamITPO } from "./components/for_exam/ITPO";
-import { ForExamThermoDynamics } from "./components/for_exam/thermodynamics";
-import { FinalDrillingEngineering } from "./components/subjects/drillingEngineering";
-import { FinalITPO } from "./components/subjects/itpo-drilling";
 
 export const App = () => {
   return (
     <div className="mx-[10%] my-20 flex flex-col gap-y-10">
-      <ForExamDrillingEnginneering />
+      <ForExamITPO />
     </div>
   );
 };
--- src/components/for_exam/ITPO.tsx
+++ src/components/for_exam/ITPO.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Essay } from "../question/essay";
+import { MultipleInputQuestion } from "../question/multipleInputQuestion";
 import { OneLineQuestion } from "../question/oneLineQues";
 import { TrueOrFalse } from "../question/trueOrFalse";
 import { JumbleQuestions } from "../utils";
@@ -80,10 +81,161 @@ const Lec3 = () => {
   );
 };
 
+const Lec4 = () => {
+  return (
+    <>
+      <Essay
+        question="Write about biogenic theory"
+        points={[
+          [
+            "According to biogenic theory",
+            " ",
+            "and",
+            "found in formation are product of",
+            "and",
+            "term process that acted on",
+            "of",
+            "such as",
+            "and",
+            "that lived millions years of ago in",
+            "",
+          ],
+          [
+            "Upon",
+            ", the organisms formed",
+            ".",
+            "and",
+            "were formed by",
+            "action in the",
+            "condition of the",
+            "and subsequent",
+            "that occurred after the",
+            "was buried under many additional",
+            "",
+          ],
+          ["Those", "process break", "molecules into", "molecules"],
+        ]}
+        answer={[
+          [
+            "Hydrocarbon",
+            "gases",
+            "liquids",
+            "short",
+            "long",
+            "remants",
+            "organism",
+            "algae",
+            "plankton",
+            "aqueous environments",
+          ],
+          [
+            "death",
+            "organic rich sediments",
+            "Oil",
+            "gas",
+            "bacterial",
+            "anaerobic",
+            "sediment",
+            "thermal process",
+            "sediment",
+            "layers of sediments",
+          ],
+          ["thermal", "larger", "smaller"],
+        ]}
+      />
+      <MultipleInputQuestion
+        question="What are two types of reservoir"
+        answer={["Unconventional", "Conventional"]}
+      />
+      <OneLineQuestion
+        question="In unconventional reservoir, from what rock the hydrocarbon is extracted"
+        answer="Source Rock"
+      />
+      <OneLineQuestion
+        question="In conventional reservoir, from what rock the hydrocarbon is extracted"
+        answer="Trap rock"
+      />
+      <JumbleQuestions>
+        <TrueOrFalse
+          question="Unconventional Reservoir is easy to extract"
+          answer={false}
+        />
+        <TrueOrFalse
+          question="Unconventional Reservoir is difficult to extract"
+          answer={true}
+        />
+        <TrueOrFalse
+          question="Conventional Reservoir is difficult to extract"
+          answer={false}
+        />
+        <TrueOrFalse
+          question="Conventional Reservoir is easy to extract"
+          answer={true}
+        />
+      </JumbleQuestions>
+      <JumbleQuestions>
+        <OneLineQuestion
+          question="What is primary migration"
+          answer="Movement of petroleum from source rock toward reservoir rock"
+        />
+        <OneLineQuestion
+          question="What is secondary migration"
+          answer="Movement of petroleum from coarse grained carrier bed to a reservoir rock"
+        />
+      </JumbleQuestions>
+      <JumbleQuestions>
+        <MultipleInputQuestion
+          question="What are types of unconventional resources"
+          answer={[
+            "Low perm oil",
+            "Tight gas sands",
+            "Gas shales",
+            "Heavy oil",
+            "Coal gas",
+            "Gas hydrates",
+            "Oil shales",
+          ]}
+        />
+        <MultipleInputQuestion
+          question="What are types of conventional resources"
+          answer={["High Quality", "Medium quality"]}
+        />
+      </JumbleQuestions>
+      <JumbleQuestions>
+        <OneLineQuestion
+          question="Explain conventional resources"
+          answer="Resources that can be produced at economic flow rates or that produce economic volumes of oil and gas without stimulation treatments or special recovery process and technologies"
+        />
+        <OneLineQuestion
+          question="Explain unconventional resources"
+          answer="Resources that cannot be produced at economic flow rates or do not produce economic volumes of oil and gas without stimulation treatments or special recovery process and technologies"
+        />
+      </JumbleQuestions>
+      <OneLineQuestion
+        question="How many barrels are in Giant oil field"
+        answer="500 million barrels to 5 billion barrels"
+      />
+      <OneLineQuestion
+        question="How many barrels are in Supergiant oil field"
+        answer="More than 5 billion barrels"
+      />
+      <OneLineQuestion
+        question="How many volume of gas are in giant gas field"
+        answer="3 to 30 trillion ft3"
+      />
+      <OneLineQuestion
+        question="How many volume of gas are in Supergiant gas field"
+        answer="More than 30 trillion ft3"
+      />
+    </>
+  );
+};
+
 export const ForExamITPO = () => {
   return (
     <>
       <Lec3 />
+      <Lec4 />
     </>
   );
 };
